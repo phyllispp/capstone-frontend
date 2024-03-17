98a1e9e08e5463cd7628faac6cc81681ed9ac9d0
phyllispp <hongyu.pann@gmail.com>
2024-03-17 14:07:58 -0400

    commit before adding Karen's code

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
@@ -7,8 +8,18 @@ import "./index.css";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_APP_CLIENT_ID}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_APP_AUDIENCE,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
